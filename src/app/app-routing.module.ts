0f9356e50647296a13923e6356ffb45957e70bb9
insurance123 <generalinsurancelti@gmail.com>
2021-04-19 09:22:13 +0530

    merged files of angular with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuyVehicleComponent } from './buy-vehicle/buy-vehicle.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RenewVehicleComponent } from './renew-vehicle/renew-vehicle.component';
 
 const routes: Routes = [
   {
@@ -13,7 +15,14 @@ const routes: Routes = [
   },
   {
     path: "register", component:RegisterComponent
+  },
+  {
+    path: "buyvehicle", component:BuyVehicleComponent
+  },
+  {
+    path: "renewvehicle", component:RenewVehicleComponent
   }
+
 ];
 
 @NgModule({
